4c1a03019b289c98c9ca24a3402713650bcfd95f
Jacob Stanger <jstangwork@gmail.com>
2020-04-03 16:58:32 -0600

    started the server.js file

---
+++ server.js
@@ -0,0 +1,34 @@
+// ***************************************************************************
+// Server.js - passMAN | the password manager for all
+// ***************************************************************************
+// ** Dependencies **
+const express = require("express");
+
+
+// sets up the express app
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static Directory
+app.use(express.static("public"));
+
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+
+
+// Syncing our sequelize models and then starting our express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+});
